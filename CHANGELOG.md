8538249468516acb4bde612fa45d3a33a832bca2
junddao <y2hunter@gmail.com>
2024-04-19 16:03:07 +0900

    First commit

---
+++ CHANGELOG.md
@@ -0,0 +1,12 @@
+# 0.1.0+1
+
+- init
+
+# 0.1.0+2
+
+- Fix
+
+# 1.0.0+5
+
+- Added Core Boilerplate
+- Added Test Folder Structure
---
+++ __brick__/lib/index.dart
@@ -0,0 +1,9 @@
+// dart
+export 'dart:convert';
+export 'dart:developer' hide Flow;
+export 'dart:io';
+
+export 'package:flutter/material.dart';
+
+export 'core/core.dart';
+export 'feature/feature.dart';
